b46bc3671ec593230b611a1ee4f50e6f76cf081c
babyplus <maxcrazy@yeah.net>
2021-11-10 04:01:47 +0000

    clean

--- scripts/tools.sh
+++
@@ -1,8 +0,0 @@
-
-function iping()
-{
-    source_dir=$1
-    destination_dir=$2
-    work_dir=$3
-    docker run --net host -it --rm -w $work_dir -v $source_dir:$destination_dir $image ansible-playbook ping.yml -v
-}
