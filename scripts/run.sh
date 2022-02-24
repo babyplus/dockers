38d62ecdb235b97dd50eefe8d7249677dc90ab9f
babyplus <maxcrazy@yeah.net>
2022-02-24 02:41:53 +0000

    Startup the Redis before starting the software

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,10 @@
+docker rm redis
+docker run -p 127.0.0.1:6379:6379 --name redis -d redis redis-server --save 60 1 --loglevel warning
+docker run -it --network host --rm redis redis-cli ping | grep PONG
+[ 0 -eq $? ] || {
+    echo Redis is not working properly.
+    exit 1
+}
 bash generate_code.sh &&\
 bash extend_code.sh &&\
 bash test_code.sh
