a72b353b201b261b9368f5158587c262814dacf5
babyplus <maxcrazy@yeah.net>
2022-02-25 09:05:17 +0000

    Run container in background

---
+++ normal.sh
@@ -0,0 +1,6 @@
+original_path=`pwd`
+exec_path=`dirname $0`
+cd $exec_path
+data_path=${1:-/tmp}
+docker run -it -p 127.0.0.1:8000:8000 -v $data_path:/tmp --rm python:3-alpine python -m http.server -d /tmp
+cd $original_path
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ original_path=`pwd`
 exec_path=`dirname $0`
 cd $exec_path
 data_path=${1:-/tmp}
-docker run -it -p 127.0.0.1:8000:8000 -v $data_path:/tmp --rm python:3-alpine python -m http.server -d /tmp
+docker run -d -p 127.0.0.1:8000:8000 -v $data_path:/tmp --rm python:3-alpine python -m http.server -d /tmp
 cd $original_path
