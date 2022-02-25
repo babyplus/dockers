2ab7bf7a16ce6540178120a8cac020aeab6b6156
babyplus <maxcrazy@yeah.net>
2022-02-25 08:53:47 +0000

    Run container in background

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
+original_path=`pwd`
+exec_path=`dirname $0`
+cd $exec_path
 path=`pwd`
-docker run --net host -v $path/conf.d:/etc/nginx/conf.d -v $path/nginx.conf:/etc/nginx/nginx.conf nginx
+docker run -d --rm --name reverse_proxy_$RANDOM --net host -v $path/conf.d:/etc/nginx/conf.d -v $path/nginx.conf:/etc/nginx/nginx.conf nginx
+cd $original_path
