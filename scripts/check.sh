88bb73d183341a9825960feb5e3aad7e916a329c
babyplus <maxcrazy@yeah.net>
2022-03-01 07:59:12 +0000

    inspection

--- scripts/check.sh
+++ scripts/check.sh
@@ -3,7 +3,7 @@ docker run --net host --rm -w /etc/ansible/yml -v $path/etc/ansible:/etc/ansible
 [ ! 0 -eq $? ] || {
     exit 1
 }
-ps -ef | grep repeated_ping.sh
+ps -ef | grep -v grep | grep repeated_ping.sh
 [ ! 0 -eq $? ] || {
     exit 2
 }
